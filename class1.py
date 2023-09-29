4690206d83f263435679a6a3986d78dcb685f8b0
KasShah <kasievashahi599@gmail.com>
2023-09-29 18:36:36 +0600

    'Hw8'

---
+++ class1.py
@@ -0,0 +1,12 @@
+class Hello:
+
+    def __init__(self, name):
+        self.name = name
+
+
+class Hi(Hello):
+    def __init__(self, name):
+        super().__init__(name)
+
+    def __str__(self):
+        return self.name
--- task.py
+++ task.py
@@ -1,3 +1,10 @@
 from art import tprint
+from class1 import Hi
+
+
 tprint("PYTHON")
+
+hi = Hi('Airo')
+
+tprint(hi.name)
 
